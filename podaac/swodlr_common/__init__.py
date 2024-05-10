8497dfe69fc80fb684fc9d98b365f9797982a629
Joshua Garde <jgarde@jpl.nasa.gov>
2024-05-09 19:33:37 -0700

    Cleanup

--- podaac/swodlr_common/__init__.py
+++ podaac/swodlr_common/__init__.py
@@ -1,8 +1,5 @@
 import logging as pylogging
 from .logging import JsonFormatter
 
-stream_handler = pylogging.StreamHandler()
-stream_handler.setFormatter(JsonFormatter())
-
 for handler in pylogging.getLogger().handlers:
     handler.setFormatter(JsonFormatter())
