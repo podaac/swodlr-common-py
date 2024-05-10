57fa0822f2b2e995f39b9dcf388c38c557335533
Joshua Garde <jgarde@jpl.nasa.gov>
2024-05-09 18:57:26 -0700

    Debug logging

--- podaac/swodlr_common/__init__.py
+++ podaac/swodlr_common/__init__.py
@@ -4,6 +4,8 @@ from .logging import JsonFormatter
 stream_handler = pylogging.StreamHandler()
 stream_handler.setFormatter(JsonFormatter())
 
+print(pylogging.root)
 pylogging.basicConfig(
-    handlers=(stream_handler,)
+    handlers=(stream_handler,),
+    force=True
 )
